d43c04d7e3877b45357db0d65565c862783445c8
JingKrit <kritsadal64@nu.ac.th>
2024-02-04 16:11:44 +0700

    edit server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
@@ -6,7 +7,7 @@ app.use(express.json())
 app.use(cors("*"))
 
 app.get('/', (req, res) => {
-    res.json({ massage: 'Hello World!' })
+    res.json({ message: 'Hello World!' })
 })
 
 app.listen(port, () => {
